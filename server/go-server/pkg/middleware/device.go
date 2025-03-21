f47f5a53ea506db19ffb2052fb025bcc6a5270b5
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-03-21 10:54:01 +0800

    add descripe

--- server/go-server/pkg/middleware/device.go
+++ server/go-server/pkg/middleware/device.go
@@ -1,3 +1,13 @@
+// ****************************************************************************
+//
+// @file       device.go
+// @brief      设备中间件
+//
+// @author     KBchulan
+// @date       2025/03/21
+// @history
+// ****************************************************************************
+
 package middleware
 
 import (
@@ -11,7 +21,7 @@ import (
 // DeviceMiddleware 处理设备ID的中间件
 func DeviceMiddleware(novelService *service.NovelService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 1. 首先尝试从请求头获取设备ID
+		// 首先尝试从请求头获取设备ID
 		deviceID := c.GetHeader("X-Device-ID")
 		clientIP := c.ClientIP()
 		userAgent := c.Request.UserAgent()
@@ -20,13 +30,13 @@ func DeviceMiddleware(novelService *service.NovelService) gin.HandlerFunc {
 		var err error
 
 		if deviceID != "" {
-			// 2. 如果请求头中有设备ID，使用该ID获取或创建设备信息
+			// 如果请求头中有设备ID，使用该ID获取或创建设备信息
 			device, err = novelService.GetOrCreateDevice(c.Request.Context(), deviceID, clientIP, userAgent)
 		} else {
-			// 3. 如果请求头中没有设备ID，先尝试通过IP查找已存在的设备
+			// 如果请求头中没有设备ID，先尝试通过IP查找已存在的设备
 			device, err = novelService.FindDeviceByIP(c.Request.Context(), clientIP)
 			if err != nil || device == nil {
-				// 4. 如果通过IP找不到设备，创建新的设备（使用UUID）
+				// 如果通过IP找不到设备，创建新的设备（使用UUID）
 				device, err = novelService.CreateNewDevice(c.Request.Context(), clientIP, userAgent)
 			}
 		}
