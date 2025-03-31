0e474904fb5e28a6ba9bd54109ff22da20794ebd
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-03-31 20:54:53 +0800

    update

--- server/go-server/pkg/middleware/device.go
+++ server/go-server/pkg/middleware/device.go
@@ -21,7 +21,6 @@ import (
 // DeviceMiddleware 处理设备ID的中间件
 func DeviceMiddleware(novelService *service.NovelService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 首先尝试从请求头获取设备ID
 		deviceID := c.GetHeader("X-Device-ID")
 		clientIP := c.ClientIP()
 		userAgent := c.Request.UserAgent()
@@ -30,13 +29,10 @@ func DeviceMiddleware(novelService *service.NovelService) gin.HandlerFunc {
 		var err error
 
 		if deviceID != "" {
-			// 如果请求头中有设备ID，使用该ID获取或创建设备信息
 			device, err = novelService.GetOrCreateDevice(c.Request.Context(), deviceID, clientIP, userAgent)
 		} else {
-			// 如果请求头中没有设备ID，先尝试通过IP查找已存在的设备
 			device, err = novelService.FindDeviceByIP(c.Request.Context(), clientIP)
 			if err != nil || device == nil {
-				// 如果通过IP找不到设备，创建新的设备（使用UUID）
 				device, err = novelService.CreateNewDevice(c.Request.Context(), clientIP, userAgent)
 			}
 		}
@@ -47,11 +43,9 @@ func DeviceMiddleware(novelService *service.NovelService) gin.HandlerFunc {
 			return
 		}
 
-		// 将设备信息存储到上下文中
 		c.Set("deviceID", device.ID)
 		c.Set("device", device)
 
-		// 在响应头中返回设备ID，确保客户端知道其设备ID
 		c.Header("X-Device-ID", device.ID)
 
 		c.Next()
